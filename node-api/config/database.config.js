840cd4e7c4e49644d525c69757596cea3a77dbc8
Fabiano <fabiano-smi@hotmail.com>
2023-12-06 12:34:39 -0300

    #main

--- node-api/config/database.config.js
+++ node-api/config/database.config.js
@@ -1,17 +1,6 @@
-const {
-  DB_USER = "root",
-  DB_PASSWORD = "root",
-  DB_HOST = "localhost",
-  DB_PORT = "27017",
-  DB_NAME = "db_fabiano",
-} = process.env;
 
 module.exports = {
-  url: `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`,
+  url: `mongodb://root:root@localhost:27017/db_fabiano?authSource=admin`,
 };
 
-// DB_HOST=localhost
-// DB_USER=root
-// DB_PASSWORD=123456
-// DB_NAME=db_fabiano
-// DB_PORT=27017
+
